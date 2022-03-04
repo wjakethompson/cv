4b1f0d287503ae0be2f189b73e2709bcb5c87b46
Jake Thompson <wjakethompson@gmail.com>
2022-03-04 15:57:16 -0600

    add spellcheck

---
+++ check-spelling.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(spelling)
+
+ignore_words <- read_csv("spelling-whitelist.csv",
+                         col_types = cols(word = col_character())) %>% 
+  pull(word)
+
+spell_check_files("index.html", ignore = ignore_words)
+
+pagedown::chrome_print("index.html")
